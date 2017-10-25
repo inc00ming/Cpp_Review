0c9efd5e2b6e9745a18cc8d845a459fb78718667
Furkan Buyrukoglu <furkanbuyrukoglu@gmail.com>
2017-10-25 22:43:07 +0300

    Categories added

---
+++ Arrays/Array Manipulation.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    int n;
+    int m;
+    cin >> n >> m;
+    vector<long long int> vec(n,0);
+    //int minLeft = 10000001;
+    long long int max= 0, sum = 0;
+    for(int a0 = 0; a0 < m; a0++){
+        int a;
+        int b;
+        int k;
+        cin >> a >> b >> k;
+        vec[a-1] += k;
+        if(b <= n-1 )
+            vec[b] -= k;
+    }
+    
+    for(int i = 0; i<vec.size();i++) {
+        sum += vec[i];
+        if(sum>max)
+            max = sum;
+    }
+        
+    cout << max << endl;
+    //cout << minLeft << endl << maxRight << endl;
+    return 0;
+}
+
